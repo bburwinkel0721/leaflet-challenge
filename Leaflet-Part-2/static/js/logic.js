cb035e56ee2c1c8cce163e7ceef43dd41d148d6f
bburwinkel <burwinkelben1@gmail.com>
2024-06-08 10:33:47 -0400

    cleaned up comments in part 2

--- Leaflet-Part-2/static/js/logic.js
+++ Leaflet-Part-2/static/js/logic.js
@@ -1,5 +1,6 @@
 // Store our API endpoint as queryUrl.
-let queryUrl ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let queryUrl =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL.
 d3.json(queryUrl).then(function (data) {
@@ -28,34 +29,37 @@ function createMap(earthquakes) {
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
   });
 
-  let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
-  });
+  let Esri_WorldGrayCanvas = L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution: "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ",
+      maxZoom: 16,
+    }
+  );
 
   // Create a baseMaps object.
   let baseMaps = {
     "Street Map": street,
     "Topographic Map": topo,
-    "Gray Map": Esri_WorldGrayCanvas
+    "Gray Map": Esri_WorldGrayCanvas,
   };
 
   // Create a color scale based on depth
   let getColor = (depth) => {
-        if (depth > 90 ){
-            return '#d73027' 
-        } else if (depth > 70){
-            return '#fc8d59'
-        } else if (depth > 50){
-            return '#fee08b'
-        } else if (depth > 30){
-            return '#d9ef8b'
-        } else if (depth > 10){
-            return '#91cf60'
-        } else {
-            return '#1a9850'
-        }
-        };
+    if (depth > 90) {
+      return "#d73027";
+    } else if (depth > 70) {
+      return "#fc8d59";
+    } else if (depth > 50) {
+      return "#fee08b";
+    } else if (depth > 30) {
+      return "#d9ef8b";
+    } else if (depth > 10) {
+      return "#91cf60";
+    } else {
+      return "#1a9850";
+    }
+  };
 
   // Create an overlays object.
   let markerArray = earthquakes.map((earthquake) => {
@@ -71,8 +75,8 @@ function createMap(earthquakes) {
       fillOpacity: 0.5,
       radius: mag * 20000,
     });
-    
-    // Bind opup with earthquake infomation
+
+    // Bind popup with earthquake infomation
     place.bindPopup(`
       <h3>${earthquake.properties.place}</h3>
       <hr>
@@ -81,23 +85,23 @@ function createMap(earthquakes) {
       <p><strong>Time:</strong> ${new Date(earthquake.properties.time)}</p>
     `);
     // Add a tooltip with a brief summary
-    place.bindTooltip(`Magnitude: ${mag}, location: ${earthquake.properties.place}, Depth: ${depth} km`, {permanent: false});
+    place.bindTooltip(
+      `Magnitude: ${mag}, location: ${earthquake.properties.place}, Depth: ${depth} km`,
+      { permanent: false }
+    );
 
     return place;
   });
 
+  // Create a layer group for the earthquake data
   const markerLayer = L.layerGroup(markerArray);
 
-
-
-
-
+  // Create map overlay
   const overlayMaps = {
-    'Earthquakes': markerLayer,
+    Earthquakes: markerLayer,
   };
 
   // Create a new map.
-  // Edit the code to add the earthquake data to the layers.
   let myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 4,
@@ -105,35 +109,37 @@ function createMap(earthquakes) {
   });
 
   // Create a layer control that contains our baseMaps.
-  // Be sure to add an overlay Layer that contains the earthquake GeoJSON.
   let layerControl = L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    })
+    collapsed: false,
+  });
   layerControl.addTo(myMap);
 
   // Create a legend control
-  let legend = L.control({ position: 'bottomright' });
-
+  let legend = L.control({ position: "bottomright" });
   legend.onAdd = function (map) {
-    let div = L.DomUtil.create('div', 'info legend'),
-        grades = [-10, 10, 30, 50, 70, 90],
-        labels = [];
+    let div = L.DomUtil.create("div", "info legend"),
+      grades = [-10, 10, 30, 50, 70, 90],
+      labels = [];
 
     // Loop through our density intervals and generate a label with a colored square for each interval.
     for (let i = 0; i < grades.length; i++) {
       div.innerHTML +=
-        '<div><i style="background-color:' + getColor(grades[i] + 1) + '"></i> ' +
-        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '</div>' : '+</div>');
+        '<div><i style="background-color:' +
+        getColor(grades[i] + 1) +
+        '"></i> ' +
+        grades[i] +
+        (grades[i + 1] ? "&ndash;" + grades[i + 1] + "</div>" : "+</div>");
     }
 
     return div;
   };
 
+  // Add legend to map
   legend.addTo(myMap);
 }
 
 // CSS for the legend
-let style = document.createElement('style');
+let style = document.createElement("style");
 style.innerHTML = `
   .info.legend {
     background: white;
